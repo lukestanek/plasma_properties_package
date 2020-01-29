1f50be8aa4f0faf31d8311e3d8d476455f30bd2a
lukestanek <staneklu@msu.edu>
2020-01-29 09:55:30 -0500

    RTD theme testing

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Lucas J. Stanek, Liam G. Stanton, Michael S. Murillo'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 autodoc_mock_imports = ['numpy', 'matplotlib']
 
